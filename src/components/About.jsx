104f0a38f9d8a3879f31d19e34ee6ecae1e50909
yummyweb <antriksh.timepass@gmail.com>
2023-05-09 22:29:45 +0530

    all

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ export const About = () => {
         <div className="about">
             <div className="up">
                 <h1 className="title">Come chill, grab some $BUD, <span className="highlight">have a Magic Toke.</span></h1>
-                <div className="description">GUAC is for the people, for the memes, for the toast, and the chips. It's the age of avocados and the freshest memecoin.</div>
+                <div className="description">Magic $BUD Token is for people that enjoy to toke only from the smoothest grass and most magical $BUD.</div>
             </div>
             <div className="down"></div>
         </div>
